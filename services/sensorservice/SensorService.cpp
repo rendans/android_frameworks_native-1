1dc24e69504a01e1e21a571234cdd509de1ed70c
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-12-15 18:12:17 +0000

    sensorservice: Fix init sequence for pre-1.1 API sensor HALs
    The sensors API introduced a new flush() method that older binaries
    won't have. For those, replace the firstFlush invocation with a
    setDelay call since a lot of implementations interpreted that as
    the initialization step.
    We also don't want any kind of call to flush() to happen when it
    isn't there, since it'll either hit a random OEM extension or
    a memory address we really don't want to execute.
    
    Change-Id: I26ce923fe385751fed7d1c483a53c074249f0620
    
    sensorservice: Set the rate for pre-1.1 HALs _after_ activation
    
    We want to be sure these are applied in the same sequence they
    used to be...
    
    Change-Id: Ied2ba08ed3c4bed3a80bac6ab5471fcea9ba2c09
    
    Conflicts:
    	services/sensorservice/SensorService.cpp

--- services/sensorservice/SensorService.cpp
+++ services/sensorservice/SensorService.cpp
@@ -912,23 +912,21 @@ status_t SensorService::enable(const sp<SensorEventConnection>& connection,
     status_t err = sensor->batch(connection.get(), handle, 0, samplingPeriodNs,
                                  maxBatchReportLatencyNs);
 
-    // Call flush() before calling activate() on the sensor. Wait for a first
-    // flush complete event before sending events on this connection. Ignore
-    // one-shot sensors which don't support flush(). Ignore on-change sensors
-    // to maintain the on-change logic (any on-change events except the initial
-    // one should be trigger by a change in value). Also if this sensor isn't
-    // already active, don't call flush().
-    if (err == NO_ERROR &&
-            sensor->getSensor().getReportingMode() != AREPORTING_MODE_ONE_SHOT &&
-            sensor->getSensor().getReportingMode() != AREPORTING_MODE_ON_CHANGE &&
+    // Call flush() before calling activate() on the sensor. Wait for a first flush complete
+    // event before sending events on this connection. Ignore one-shot sensors which don't
+    // support flush(). Also if this sensor isn't already active, don't call flush().
+    const SensorDevice& device(SensorDevice::getInstance());
+    if (err == NO_ERROR && sensor->getSensor().getReportingMode() != AREPORTING_MODE_ONE_SHOT &&
             rec->getNumConnections() > 1) {
-        connection->setFirstFlushPending(handle, true);
-        status_t err_flush = sensor->flush(connection.get(), handle);
-        // Flush may return error if the underlying h/w sensor uses an older HAL.
-        if (err_flush == NO_ERROR) {
-            rec->addPendingFlushConnection(connection.get());
-        } else {
-            connection->setFirstFlushPending(handle, false);
+        if (device.getHalDeviceVersion() >= SENSORS_DEVICE_API_VERSION_1_1) {
+            connection->setFirstFlushPending(handle, true);
+            status_t err_flush = sensor->flush(connection.get(), handle);
+            // Flush may return error if the underlying h/w sensor uses an older HAL.
+            if (err_flush == NO_ERROR) {
+                rec->addPendingFlushConnection(connection.get());
+            } else {
+                connection->setFirstFlushPending(handle, false);
+            }
         }
     }
 
@@ -952,6 +950,11 @@ status_t SensorService::enable(const sp<SensorEventConnection>& connection,
         reg_info.mMin = timeinfo->tm_min;
         reg_info.mSec = timeinfo->tm_sec;
         mNextSensorRegIndex = (mNextSensorRegIndex + 1) % SENSOR_REGISTRATIONS_BUF_SIZE;
+    }
+
+    if (device.getHalDeviceVersion() < SENSORS_DEVICE_API_VERSION_1_1) {
+        // Pre-1.1 sensor HALs had no flush method, and relied on setDelay at init
+        sensor->setDelay(connection.get(), handle, samplingPeriodNs);
     }
 
     if (err != NO_ERROR) {
